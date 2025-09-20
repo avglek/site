532097af73732fafb3899335e4626e425ba99ac3
Aleksandr Glek <avglek@gmail.com>
2025-09-20 09:48:36 +0300

    add logo

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -9,4 +9,15 @@ import { Phone } from '../../library/components/phone/phone';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  private _firmName = 'КОНТУРСВЕТ';
+  private _phone = '+7(812)715-05-05';
+
+  get firmName(): string {
+    return this._firmName;
+  }
+
+  get phoneNumber(): string {
+    return this._phone;
+  }
+}
--- src/app/library/components/phone/phone.ts
+++ src/app/library/components/phone/phone.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-phone',
   imports: [],
   templateUrl: './phone.html',
-  styleUrl: './phone.scss'
+  styleUrl: './phone.scss',
 })
 export class Phone {
-
+  @Input() phone: string = '+7(921)888-88-88';
 }
