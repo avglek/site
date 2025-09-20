32a31c7063777a6e3d2ac1adf0ad8e7a146c152e
Aleksandr Glek <avglek@gmail.com>
2025-09-20 08:43:41 +0300

    header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { Title } from '../../library/components/title/title';
+import { Menu } from '../menu/menu';
+import { Phone } from '../../library/components/phone/phone';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [Title, Menu, Phone],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrl: './header.scss',
 })
-export class Header {
-
-}
+export class Header {}
